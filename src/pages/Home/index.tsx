48710c5b27a5ac65318562f20e0448f1ea9c6510
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2023-02-07 19:53:09 -0300

    feat(home): implements details

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,32 @@
-const Home = () => {
-    return(<div>Componente Home</div>);
-  }
+import * as S from "./style";
+import { AiOutlineSearch } from "react-icons/ai";
+import Menu from "components/Menu";
+import { RoutePath } from "types/routes";
+import { navigationItems } from "data/navigation";
 
-  export default Home;
-  
+const Home = () => {
+  return (
+    <S.Home>
+      <Menu active={RoutePath.HOME} navItems={navigationItems} />
+      <S.HomeContent>
+        <header>
+          <S.HomeHeaderDetails>
+            <div>
+              <S.HomeHeaderDetailsLogo>SAM</S.HomeHeaderDetailsLogo>
+            </div>
+            <S.HomeHeaderDetailsSearch>
+              <AiOutlineSearch />
+              <input type="text" placeholder="Procure pelo paciente" />
+            </S.HomeHeaderDetailsSearch>
+          </S.HomeHeaderDetails>
+        </header>
+        <div>
+          <S.HomeTitle>Pacientes</S.HomeTitle>
+          <S.HomePatientList><p>lista de produtos</p></S.HomePatientList>
+        </div>
+      </S.HomeContent>
+    </S.Home>
+  );
+};
+
+export default Home;
