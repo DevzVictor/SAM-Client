56e07f575ef85eee5e2d0a149cc41dd361887955
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2023-02-07 15:16:03 -0300

    feat(routers): implements routers config

---
+++ src/types/routes.ts
@@ -0,0 +1,11 @@
+// Ele será responsável por guardar os `alias` que fazem referência às rotas da nossa aplicação, com o seguinte código:
+export enum RoutePath {
+  HOME = "/",
+  LOGIN = "/login",
+  SIGNUP = "/signup",
+  SETTINGS = "/settings",
+  SETTINGS_PATIENTS = "patients",
+  SETTINGS_MEDICAMENTS = "medicaments",
+  SETTINGS_EXAMS = "exams",
+  SETTINGS_VACCINES = "vaccines",
+}
