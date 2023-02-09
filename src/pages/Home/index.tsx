db4c9f581add3420e63cc18fc62431c969733e0e
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2023-02-09 14:39:26 -0300

    feat(overlay): create component overlay

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,14 @@
+//style and icons for react-icons
 import * as S from "./style";
 import { AiOutlineSearch } from "react-icons/ai";
+
+//components
 import Menu from "components/Menu";
 import PatientList from "components/PatientList";
 import PatientCard from "components/PatientCard";
+import Overlay from "components/Overlay";
+
+//routes
 import { RoutePath } from "types/routes";
 import { navigationItems } from "data/navigation";
 
@@ -31,6 +37,7 @@ const Home = () => {
           </S.HomePatientList>
         </div>
       </S.HomeContent>
+      {/* <Overlay></Overlay> */}
     </S.Home>
   );
 };
